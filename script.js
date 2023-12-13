7952e984370d3db3b5a839b64f154dcc66a3ef84
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2023-12-13 01:29:03 +0000

    updated script.js to allow todos to save locally

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const todoItems = []
+let todoItems = []
 const todoArea = document.querySelector('.todos')
 const todoCount = document.querySelector('#counter')
+
+initTodoWidget()
 
 function addTodo() {
     const inputElement = document.querySelector('#todo-input')
 
     todoItems.push(inputElement.value)
+
+    saveLocally(todoItems)
 
     inputElement.value = ''
 
@@ -18,17 +22,18 @@ function renderTodo() {
     let todoAllHTML = ''
     for (let i = 0; i < todoItems.length; i++) {
         const html = `
-            <div class='todo-line' 
-                onclick='
-                    todoItems.splice(${i},1); 
-                    renderTodo(); 
-                    updateCount();
-                '
-            >
-                <button class='todo-check'></button>
-                <p>${todoItems[i]}</p>
-            </div>
-        `
+                <div class='todo-line' 
+                    onclick='
+                        todoItems.splice(${i},1);
+                        saveLocally(todoItems)
+                        renderTodo(); 
+                        updateCount();
+                    '
+                >
+                    <button class='todo-check'></button>
+                    <p>${todoItems[i]}</p>
+                </div>
+            `
         todoAllHTML += html
     }
     todoArea.innerHTML = todoAllHTML
@@ -36,4 +41,32 @@ function renderTodo() {
 
 function updateCount() {
     todoCount.innerHTML = todoItems.length
+}
+
+function saveLocally(todos) {
+    if (todos[0] !== '' || todos.length === 0 || todos === null) {
+        localStorage.setItem('todos', todos)
+    }
+}
+
+function getLocallySaved() {
+    try {
+        todoItems = [...(localStorage.getItem('todos').split(','))]
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+function resetTodo() {
+    if (todoItems[0] === '') {
+        localStorage.clear()
+        todoItems.pop()
+    }
+}
+
+function initTodoWidget() {
+    getLocallySaved()
+    resetTodo()
+    updateCount()
+    renderTodo()
 }
